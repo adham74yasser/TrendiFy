989272fa7dee547f91a8a06f1c50d1e29b3f45be
adham74yasser <ama.m.13.8.2007@gmail.com>
2026-02-03 02:19:08 +0200

    storage

--- js/main_pro.js
+++ js/main_pro.js
@@ -80,8 +80,28 @@ if(data.length === 0){
 function active(activeImg) {
    imgSrc.src = activeImg.src         
 }
+
+
+//
+let img_1 = document.getElementById('img_1')
+let img_2 = document.getElementById('img_2')
+let img_3 = document.getElementById('img_3')
+let img_4 = document.getElementById('img_4')
+
+
+let imagSrc = localStorage.getItem('src');
+let imagSrc_1 = localStorage.getItem('src_1');
+let imagSrc_2 = localStorage.getItem('src_2');
+let imagSrc_3 = localStorage.getItem('src_3');
+let imagSrc_4 = localStorage.getItem('src_4');
+
+if (imagSrc) {
+    imgSrc.src = imagSrc;
+    img_1.src = imagSrc_1;
+    img_2.src = imagSrc_2;
+    img_3.src = imagSrc_3;
+    img_4.src = imagSrc_4;
 }
-
-
-
+ 
+}
 // 
--- js/main_shop.js
+++ js/main_shop.js
@@ -1,8 +1,9 @@
 let container = document.getElementById('container')
 let item = document.getElementById('item')
 let show = document.getElementById('show')
+let search = document.getElementById('search')
 
-if(container && item && show) {
+if(container && item && show && search) {
     
 let cards = [
     {
@@ -12,88 +13,118 @@ let cards = [
         title : 'Colorful Pattern <br>Shirts',
         prise : '$238.85',
         discount : '$245.8',
-        link : 'product_1.html'
+        mainImg_1 :'images/image-product/Gemini_Generated_Image_7h5ugv7h5ugv7h5u.png',
+        mainImg_2 :"images/image-product/Gemini_Generated_Image_se7xv0se7xv0se7x.png",
+        mainImg_3 :"images/image-product/Gemini_Generated_Image_tj6hqktj6hqktj6h.png",
+        mainImg_4 :"images/image-product/Gemini_Generated_Image_wg8zrjwg8zrjwg8z.png"
     },
     {
-        img_1 : 'images/product-2-1.jpg',
-        img_2 : 'images/product-2-2.jpg',
+        img_1 : 'images/image-product/photo_1_1.jpg',
+        img_2 : 'images/image-product/photo_1_2.jpg',
         head : 'Clothing',
         title : 'Fish Print Patched T-shirt',
         prise : '$199.99',
         discount : '$249.99',
-        link : 'product_2.html'
+        mainImg_1 :"images/image-product/photo_1_1.jpg",
+        mainImg_2 :"images/image-product/photo_1_2.jpg",
+        mainImg_3 :"images/image-product/photo_1_3.jpg",
+        mainImg_4 :"images/image-product/photo_1_4.jpg"
     },
     {
-        img_1 : 'images/product-3-1.jpg',
-        img_2 : 'images/product-3-2.jpg',
+        img_1 : 'images/image-product/photo_2_1.jpg',
+        img_2 : 'images/image-product/photo_2_2.jpg',
         head : 'Clothing',
         title : 'Vintage Floral Print Dress',
         prise : '$349.99',
         discount : '$299.99',
-        link : 'product_3.html'
+        mainImg_1 :"images/image-product/photo_2_1.jpg",
+        mainImg_2 :"images/image-product/photo_2_2.jpg",
+        mainImg_3 :"images/image-product/photo_2_3.jpg",
+        mainImg_4 :"images/image-product/photo_2_4.jpg"
     },
     {
-        img_1 : 'images/product-4-1.jpg',
-        img_2 : 'images/product-4-2.jpg',
+        img_1 : 'images/image-product/photo_3_1.jpg',
+        img_2 : 'images/image-product/photo_3_2.jpg',
         head : 'Clothing',
         title : 'Stripe Circle Print T-Shirt ',
         prise : '$149.99',
         discount : '$189.99',
-        link : 'product_4.html'
+        mainImg_1 :"images/image-product/photo_3_1.jpg",
+        mainImg_2 :"images/image-product/photo_3_2.jpg",
+        mainImg_3 :"images/image-product/photo_3_3.jpg",
+        mainImg_4 :"images/image-product/photo_3_4.jpg"
     },
     {
-        img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-5-1.jpg',
-        img_2 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-5-2.jpg',
+        img_1 : 'images/image-product/photo_4_1.jpg',
+        img_2 : 'images/image-product/photo_4_2.jpg',
         head : 'Clothing',
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_5.html'
+        mainImg_1 :"images/image-product/photo_4_1.jpg",
+        mainImg_2 :"images/image-product/photo_4_2.jpg",
+        mainImg_3 :"images/image-product/photo_4_3.jpg",
+        mainImg_4 :"images/image-product/photo_4_4.jpg"
     },
     {
-        img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-6-1.jpg',
-        img_2 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-6-2.jpg',
+        img_1 : 'images/image-product/photo_5_1.jpg',
+        img_2 : 'images/image-product/photo_5_2.jpg',
         head : 'Clothing',
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_6.html'
+        mainImg_1 :"images/image-product/photo_5_1.jpg",
+        mainImg_2 :"images/image-product/photo_5_2.jpg",
+        mainImg_3 :"images/image-product/photo_5_3.jpg",
+        mainImg_4 :"images/image-product/photo_5_4.jpg"
     },
     {
-        img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-7-1.jpg',
-        img_2 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-7-2.jpg',
+        img_1 : 'images/image-product/photo_6_1.jpg',
+        img_2 : 'images/image-product/photo_6_2.jpg',
         head : 'Clothing',
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_7.html'
+        mainImg_1 :"images/image-product/photo_6_1.jpg",
+        mainImg_2 :"images/image-product/photo_6_2.jpg",
+        mainImg_3 :"images/image-product/photo_6_3.jpg",
+        mainImg_4 :"images/image-product/photo_6_4.jpg"
     },
     {
-        img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-8-1.jpg',
-        img_2 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-8-2.jpg',
+        img_1 : 'images/image-product/photo_7_1.jpg',
+        img_2 : 'images/image-product/photo_7_2.jpg',
         head : 'Clothing',
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_8.html'  
+        mainImg_1 :"images/image-product/photo_7_1.jpg",
+        mainImg_2 :"images/image-product/photo_7_2.jpg",
+        mainImg_3 :"images/image-product/photo_7_3.jpg",
+        mainImg_4 :"images/image-product/photo_7_4.jpg"  
     },
     {
-        img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-9-1.jpg',
-        img_2 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-9-2.jpg',
+        img_1 : 'images/image-product/photo_8_1.jpg',
+        img_2 : 'images/image-product/photo_8_2.jpg',
         head : 'Clothing',
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_9.html' 
+        mainImg_1 :"images/image-product/photo_8_1.jpg",
+        mainImg_2 :"images/image-product/photo_8_2.jpg",
+        mainImg_3 :"images/image-product/photo_8_3.jpg",
+        mainImg_4 :"images/image-product/photo_8_4.jpg" 
     },
     {
-        img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-10-1.jpg',
-        img_2 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-10-2.jpg',
+        img_1 : 'images/image-product/photo_9_1.jpg',
+        img_2 : 'images/image-product/photo_9_2.jpg',
         head : 'Clothing',
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_10.html' 
+        mainImg_1 :"images/image-product/photo_9_1.jpg",
+        mainImg_2 :"images/image-product/photo_9_2.jpg",
+        mainImg_3 :"images/image-product/photo_9_3.jpg",
+        mainImg_4 :"images/image-product/photo_9_4.jpg" 
     },
     {
         img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-11-1.jpg',
@@ -102,7 +133,10 @@ let cards = [
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_11.html' 
+        mainImg_1 :"images/image-product/photo_9_1.jpg",
+        mainImg_2 :"images/image-product/photo_9_2.jpg",
+        mainImg_3 :"images/image-product/photo_9_3.jpg",
+        mainImg_4 :"images/image-product/photo_9_4.jpg" 
     },
     {
         img_1 : 'images/Bootstrap E-commrece - 1_30_2026 6-37-16 AM/product-12-1.jpg',
@@ -111,12 +145,24 @@ let cards = [
         title : 'Casual Cotton T-Shirt',
         prise : '$129.99',
         discount : '$159.99',
-        link : 'product_12.html' 
+        mainImg_1 :"images/image-product/photo_9_1.jpg",
+        mainImg_2 :"images/image-product/photo_9_2.jpg",
+        mainImg_3 :"images/image-product/photo_9_3.jpg",
+        mainImg_4 :"images/image-product/photo_9_4.jpg"
     },
 ]
 
-let res = ''
+function linkst(i) {
+    localStorage.setItem('src' , cards[i].img_1)
+    localStorage.setItem('src_1' , cards[i].mainImg_1)
+    localStorage.setItem('src_2' , cards[i].mainImg_2)
+    localStorage.setItem('src_3' , cards[i].mainImg_3)
+    localStorage.setItem('src_4' , cards[i].mainImg_4)
+}
 
+
+
+let res = ''
 for(let i = 0 ; i < cards.length ; i++) {
     res += `
     <div class="card flex card1">
@@ -130,7 +176,7 @@ for(let i = 0 ; i < cards.length ; i++) {
         </div>
         <div class="title_card flex">
             <p>Clothing</p>
-            <a href="${cards[i].link}" id='onSrc'>${cards[i].title}</a>
+            <a href="product_1.html" id='onSrc' onclick ='linkst(${i})'>${cards[i].title}</a>
             <b>${cards[i].prise}<del>${cards[i].discount}</del></b>
         </div>
     </div>
@@ -139,4 +185,43 @@ for(let i = 0 ; i < cards.length ; i++) {
     item.innerText = i+1
     show.innerHTML = `<i class="fa-solid fa-table-cells-large"></i>Show : ${i+1}`
 }
+
+
+search.onkeyup = function () {
+    let searchVal = search.value.trim().toLowerCase()
+    let res = ''
+    let count = 0
+
+    for (let i = 0; i < cards.length; i++) {
+        let val = cards[i].title.trim().toLowerCase()
+
+        if (val.includes(searchVal)) {
+            res += `
+            <div class="card flex card1">
+                <div class="img">
+                    <img src="${cards[i].img_1}" alt="">
+                    <img src="${cards[i].img_2}" alt="">
+                    <a href="" class="flex">
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-bag-shopping"></i>
+                    </a>
+                </div>
+                <div class="title_card flex">
+                    <p>Clothing</p>
+                    <a href="${cards[i].link}" id="onSrc">${cards[i].title}</a>
+                    <b>${cards[i].prise} <del>${cards[i].discount}</del></b>
+                </div>
+            </div>
+            `
+            count++
+        }
+    }
+
+    container.innerHTML = res
+    item.innerText = count
+    show.innerHTML = `<i class="fa-solid fa-table-cells-large"></i> Show : ${count}`
 }
+
+}
+
+
